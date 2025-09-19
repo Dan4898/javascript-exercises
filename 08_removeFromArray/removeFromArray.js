3da57215284756484876130aec8a0bad78960028
Dan4898 <danrleypereira294@gmail.com>
2025-09-19 20:38:34 -0300

    Exercćios 6 a 9 de loops e arrays The Odin Project

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,5 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...num) {
+  return arr.filter((value) => !num.includes(value));
 };
 
 // Do not edit below this line
--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function (a, b) {
+  if (a < 0 || b < 0) {
+    return "ERROR";
+  } else if (typeof a !== "number" || typeof b !== "number") {
+    return "ERROR";
+  } else if (a % 1 !== 0 || b % 1 !== 0) {
+    return "ERROR";
+  } else {
+  }
 
+  let sumNumbers = 0;
+  for (let i = Math.min(a, b); i <= Math.max(a, b); i++) {
+    sumNumbers += i;
+  }
+
+  return sumNumbers;
 };
 
 // Do not edit below this line
