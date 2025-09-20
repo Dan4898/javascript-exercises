98d18d508a1cde6eb98e90f33e1dd770ed580f07
Dan4898 <danrleypereira294@gmail.com>
2025-09-20 09:04:46 -0300

    Resolução dos exercícios 10 e 11 de JavaScript do The Odin Project

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,11 +1,25 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (F) {
+  const C = (5 / 9) * (F - 32);
+  const fixed = parseFloat(C.toFixed(1));
+  if (C % 1 !== 0) {
+    return fixed;
+  } else {
+    return C;
+  }
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (C) {
+  const F = C * 1.8 + 32;
+  const fixed = parseFloat(F.toFixed(1));
+  if (F % 1 !== 0) {
+    return fixed;
+  } else {
+    return F;
+  }
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
